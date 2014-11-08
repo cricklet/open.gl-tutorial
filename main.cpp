553344154e74824ed80f814bcf1e665c23a7261e
Kenrick Rilee <kenrick.rilee@gmail.com>
2014-11-07 21:02:50 -0500

    Error handling...

--- main.cpp
+++ main.cpp
@@ -12,6 +12,35 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
 
+bool checkErrors(const char *filename, int line) {
+  bool result = false;
+  
+  GLenum error;
+  while ((error = glGetError()) != GL_NO_ERROR) {
+    std::string str;
+ 
+    switch(error) {
+    case GL_INVALID_OPERATION:
+      str="INVALID_OPERATION"; break;
+    case GL_INVALID_ENUM:
+      str="INVALID_ENUM"; break;
+    case GL_INVALID_VALUE:
+      str="INVALID_VALUE"; break;
+    case GL_OUT_OF_MEMORY:
+      str="OUT_OF_MEMORY"; break;
+    case GL_INVALID_FRAMEBUFFER_OPERATION:
+      str="INVALID_FRAMEBUFFER_OPERATION";  break;
+    }
+ 
+    // std::cerr << "GL_" << str.c_str() << "\n";
+    std::cerr << "GL_" << str.c_str() << " - "<< filename << ":" << line << "\n";
+    result = true;
+  }
+
+  return result;
+}
+#define checkErrors() checkErrors(__FILE__, __LINE__)
+
 char *getFileContents(const char *filename) {
   FILE *file;
   long fileSize;
@@ -45,17 +74,17 @@ char *getFileContents(const char *filename) {
 
 GLuint compileShader (const char *filename, GLenum shaderType) {
   char *fileContents;
-  // try {
-  fileContents = getFileContents(filename);
-  // } catch (std::exception &e) {
-  //   std::cerr << "Exception caught: " << e.what() << "\n";
-  // }
+  try {
+    fileContents = getFileContents(filename);
+  } catch (std::exception &e) {
+    std::cerr << "Exception caught: " << e.what() << "\n";
+  }
 
   GLuint shader = glCreateShader(shaderType);
-  glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(shader, 1, &fileContents, NULL);
   glCompileShader(shader);
-
+  
+  // Print shader compile errors.
   GLint status;
   glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
   if (status != GL_TRUE) {
@@ -83,25 +112,33 @@ int main (int argv, char *argc[]) {
 
   glewExperimental = GL_TRUE; // necessary for modern opengl calls
   glewInit();
+  checkErrors();
 
-  GLuint vertexBuffer; // test that glew loaded
-  glGenBuffers(1, &vertexBuffer);
-  std::cout << vertexBuffer << "\n";
-
-  float vertices[] = {
+  GLfloat vertices[] = {
     0.0f,  0.5f, // Vertex 1 (X, Y)
     0.5f, -0.5f, // Vertex 2 (X, Y)
     -0.5f, -0.5f  // Vertex 3 (X, Y)
   };
 
-  GLuint vbo; // vertex buffer object
+  // We don't want to have to call glVertexAttribPointer to reset the inputs every time we enable
+  // a shader (glUseProgram). Instead, we can store all the state needed to use a shader inside
+  // a vertex array object.
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+  checkErrors();
+
+  // We don't want to have to copy vertices to the GPU every time we render. Instead, we can copy
+  // the vertex information into a vertex buffer object.
+  GLuint vbo;
   glGenBuffers(1, &vbo); // generate one buffer object name
   glBindBuffer(GL_ARRAY_BUFFER, // target to bind to
 	       vbo);
-
   glBufferData(GL_ARRAY_BUFFER, // target buffer object
-	       sizeof(vertices), vertices,
+	       sizeof(vertices),
+	       vertices,
 	       GL_STATIC_DRAW); // vs GL_DYNAMIC_DRAW vs GL_STREAM_DRAW
+  checkErrors();
 
   // Load the shaders from the filesystem.
   GLuint vertShader = compileShader("screen.vert", GL_VERTEX_SHADER);
@@ -109,14 +146,18 @@ int main (int argv, char *argc[]) {
   GLuint shaderProgram = glCreateProgram();
   glAttachShader(shaderProgram, vertShader);
   glAttachShader(shaderProgram, fragShader);
+  checkErrors();
 
   // Because fragment shaders can often write to multiple buffers, we need to specify which buffer
   // to bind the user-defined varying 'out' variable to.
   glBindFragDataLocation(shaderProgram,
 			 0, // color number (buffer) to bind to
 			 "outColor");
+  checkErrors();
 
   glLinkProgram(shaderProgram);
+  glUseProgram(shaderProgram);
+  checkErrors();
   
   // Shader needs to know how to get the input attributes.
   GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
@@ -126,10 +167,8 @@ int main (int argv, char *argc[]) {
 			GL_FALSE, // should be normalized
 			0,  // stride: # bytes between each attribute in the array
 			0); // offset: # bytes from the start of the array
-
   glEnableVertexAttribArray(posAttrib);
-
-  glUseProgram(shaderProgram);
+  checkErrors();
 
   SDL_Event windowEvent;
   while (true) {
@@ -139,6 +178,10 @@ int main (int argv, char *argc[]) {
 	if (windowEvent.key.keysym.sym == SDLK_ESCAPE) break;
       }
     }
+
+    glDrawArrays(GL_TRIANGLES,
+		 0,  // # vertices to skip at the beginning
+		 3); // # verticies to process
 
     SDL_GL_SwapWindow(window);
   }
